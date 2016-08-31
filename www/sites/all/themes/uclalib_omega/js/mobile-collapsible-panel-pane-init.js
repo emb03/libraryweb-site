d2c99759fd8d0ed55f46951ea32fedd4981ecdfa
James Wilson <jrwilson3@gmail.com>
2016-08-30 20:37:50 -0500

    UCLALIB-235/UCLALIB-232: Improve mobile collapsible sidebar nav theming.

--- www/sites/all/themes/uclalib_omega/js/mobile-collapsible-panel-pane-init.js
+++ www/sites/all/themes/uclalib_omega/js/mobile-collapsible-panel-pane-init.js
@@ -8,4 +8,20 @@
       });
     }
   };
+
+  // Fallback for panelized nodes that do not have the collapsible class
+  // on the uclalib_nav sidebar block.
+  Drupal.behaviors.mobileCollapsibleSideNav = {
+    attach: function (context, settings) {
+      $(context).find('.pane-uclalib-nav:not(.mobile-collapsible)').addClass('mobile-collapsible').mobileCollapsible({
+          breakpoint: "(max-width: 767px)",
+          titleSelector: ".pane-title",
+          contentSelector: ".pane-content"
+      }).addClass('mobile-collapsible-processed');
+
+      // If the top-level page in sidebar nav is the active page, then change
+      // the style to the darker blue dropdown.
+      $(context).find('.pane-uclalib-nav .pane-title a.active').closest('.pane-uclalib-nav').addClass('mobile-collapsible--blue');
+    }
+  };
 }(jQuery));
