35fb890974c90743bf7b69ef485d6934e98b8c51
James Wilson <jrwilson3@gmail.com>
2016-08-30 21:40:19 -0500

    UCLALIB-235: Collapsible Link Lists and Quick Find panes.

--- www/sites/all/themes/uclalib_omega/js/mobile-collapsible-panel-pane-init.js
+++ www/sites/all/themes/uclalib_omega/js/mobile-collapsible-panel-pane-init.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.mobileCollapsiblePanelPane = {
     attach: function (context, settings) {
       $(context).find('.panel-pane.mobile-collapsible').mobileCollapsible({
-          breakpoint: "(max-width: 480px)",
-          titleSelector: ".pane-title",
-          contentSelector: ".pane-content"
+        breakpoint: "(max-width: 480px)",
+        titleSelector: ".pane-title",
+        contentSelector: ".pane-content"
       });
     }
   };
@@ -14,14 +14,36 @@
   Drupal.behaviors.mobileCollapsibleSideNav = {
     attach: function (context, settings) {
       $(context).find('.pane-uclalib-nav:not(.mobile-collapsible)').addClass('mobile-collapsible').mobileCollapsible({
-          breakpoint: "(max-width: 767px)",
-          titleSelector: ".pane-title",
-          contentSelector: ".pane-content"
+        breakpoint: "(max-width: 767px)",
+        titleSelector: ".pane-title",
+        contentSelector: ".pane-content"
       }).addClass('mobile-collapsible-processed');
 
       // If the top-level page in sidebar nav is the active page, then change
       // the style to the darker blue dropdown.
       $(context).find('.pane-uclalib-nav .pane-title a.active').closest('.pane-uclalib-nav').addClass('mobile-collapsible--blue');
+    }
+  };
+
+  // Make link list panes in main content column collapsible on mobile.
+  Drupal.behaviors.mobileCollapsibleLinkList = {
+    attach: function (context, settings) {
+      $(context).find('.pane-bean-link-list:not(.sidebar-tier-2)').addClass('mobile-collapsible').mobileCollapsible({
+        breakpoint: "(max-width: 767px)",
+        titleSelector: ".pane-title",
+        contentSelector: ".pane-content"
+      });
+    }
+  };
+
+  // Make quick find panes collapsible on mobile.
+  Drupal.behaviors.mobileCollapsibleQuickFind = {
+    attach: function (context, settings) {
+      $(context).find('.pane-bean-quick-find').addClass('mobile-collapsible').addClass('mobile-collapsible--blue').mobileCollapsible({
+        breakpoint: "(max-width: 767px)",
+        titleSelector: ".pane-title",
+        contentSelector: ".pane-content"
+      });
     }
   };
 }(jQuery));
