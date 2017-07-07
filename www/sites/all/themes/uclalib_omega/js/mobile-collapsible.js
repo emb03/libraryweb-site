da67d8dd1fdcaf0c34a44930d29092b95c82fbe6
Casey Grzecka <c@sey.gr>
2017-07-07 11:22:42 -0700

    fixes indentation and missing }

--- www/sites/all/themes/uclalib_omega/js/mobile-collapsible.js
+++ www/sites/all/themes/uclalib_omega/js/mobile-collapsible.js
@@ -42,9 +42,10 @@
     mc.checkBreakpoint( mc.media );
 
     $(mc.element).find(mc.options.titleSelector).on('click', function (e) {
-    if (mc.media.matches) {
-      e.preventDefault();
-      mc.toggle();
+      if (mc.media.matches) {
+        e.preventDefault();
+        mc.toggle();
+      }
     });
     $el.addClass('mobile-collapsible-processed');
   }
