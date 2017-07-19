145d911638f690b08e0880eede3d73bf9a2e96c3
Casey Grzecka <c@sey.gr>
2017-07-18 18:31:54 -0700

    DRUP-1051: Open Now indicator in sidebar via ajax (#406)

--- www/sites/all/modules/custom/ucla_libhours/ucla_libhours.js
+++ www/sites/all/modules/custom/ucla_libhours/ucla_libhours.js
@@ -73,6 +73,29 @@ var libHoursApp = libHoursApp || {};
         var view = new libHoursApp.appView({el: '.'+element.classList[1]});
         Drupal.libHoursAppViews.push(view);
       });
+
+      // Open Now backbone stuff
+      var libHoursAppLocationModel = Backbone.Model.extend({
+        url: '/ajax/libhours/hours/today/'+ Drupal.settings.ucla_libhours.hours.lid
+      });
+      
+      var libHoursAppLocationOpenNowView = Backbone.View.extend({
+        el: '.open-now-pane',
+        initialize: function() {
+          this.model = new libHoursAppLocationModel();
+          this.listenTo(this.model, 'sync', this.render);
+          this.model.fetch();
+        },
+        render: function() {
+          var todays_hours = this.model.toJSON();
+          if (todays_hours.locations["0"].times.currently_open) {
+            $(this.el).html('<span class="location-open-now">Open</span>');
+          } else {
+            $(this.el).html('<span class="location-open-now">Closed</span>');
+          }
+        }
+      });
+      var open_now = new libHoursAppLocationOpenNowView();
     }
   };
 })(jQuery);
