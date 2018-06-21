5db82ead8b69c5e53c037783a888431e6d7307b1
Casey Grzecka <c@sey.gr>
2018-06-21 09:49:03 -0700

    Staff Directory: Sprint 4 deployment (#467)
    * DRUP-1282: #ready adds breadcrumbs to search results and A-Z pages
    * also adds some css spacing above h2 headers
    * and some css for staff directory page title spacing on search result and A-Z list pages
    
    * DRUP-1293: #ready adds the links to the locations on the search results profile cards
    
    * DRUP-1299: #ready Auto generates Staff by location page aliases
    
    * DRUP-1300: #ready submits the staff directory search form when clicking on the search icon

--- www/sites/all/modules/features/uclalib_staff_directory/uclalib_staff_directory.js
+++ www/sites/all/modules/features/uclalib_staff_directory/uclalib_staff_directory.js
@@ -1,1 +1,6 @@
- jQuery('#edit-submit-staff-search').hide();
+// Hide the submit search button in favor of fancy icon
+jQuery('#edit-submit-staff-search').hide();
+// Make fancy icon submit form on click/tap
+jQuery('#edit-search-api-views-fulltext-wrapper i').click(function() {
+  jQuery('#views-exposed-form-staff-search-default').submit();
+});
